8bc10d12a4b4e57e2dc04bed7655a3c54d0643f3
fabramon21 <ramon.12@osu.edu>
2025-03-06 12:44:22 -0500

    adding wrapper Auth to Root

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+import {AuthProvider } from '@/contexts/AuthContext';
+
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <AuthProvider>
+    <App />
+    </AuthProvider>
+);
